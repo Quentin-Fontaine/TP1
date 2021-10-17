207ac228a8d0aa1d605f05d0d5b0b77e9f6bc296
Charles DESGENETEZ <dorpaxione@gmail.com>
2021-10-17 15:45:01 +0200

    test

--- sudoku/src/main/java/fr/univ_montpellier/fsd/sudoku/imp/Sudoku.java
+++ sudoku/src/main/java/fr/univ_montpellier/fsd/sudoku/imp/Sudoku.java
@@ -1,59 +1,80 @@
 package fr.univ_montpellier.fsd.sudoku.imp;
 
-import java.time.Duration;
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Sudoku {
 
-	int n;
-	int s;
-	int[][] grid;
+    int n;
+    int s;
+    int[][] grid;
 
-	/*
-	 * Create an instance of the problem sudoku (nxn)
-	 * 
-	 */
+    /*
+     * Create an instance of the problem sudoku (nxn)
+     *
+     */
 
-	public Sudoku(int n) {
-		this.n = n;
-		this.s = (int) Math.sqrt(n);
-		this.grid = new int[n][n];
-	}
+    public Sudoku(int n) {
+        this.n = n;
+        this.s = (int) Math.sqrt(n);
+        this.grid = new int[n][n];
+    }
 
-	/*
-	 * check if this.grid is a correct sudoku solution.
-	 * 
-	 */
+    /*
+     * check if this.grid is a correct sudoku solution.
+     *
+     */
 
-	private boolean solutionChecker() {
-		// TODO
-		return false;
+    private boolean solutionChecker() {
+        final int total = n * (n + 1) / 2;
+        for (int i = 0; i < this.n; i++) {
+            int sum = 0;
+            final Set<Integer> rowNumbers = new HashSet<>();
+            for (int j = 0; j < this.n; j++) {
+                int number = this.grid[i][j];
+                rowNumbers.add(number);
+                sum += number;
+            }
 
-	}
+            if(rowNumbers.size() != this.n || sum != total) return false;
+        }
 
-	/*
-	 * Generate a random grid solution
-	 * 
-	 */
+        for(int j = 0; j < this.n; j++) {
+            int sum = 0;
+            final Set<Integer> colNumbers = new HashSet<>();
+            for(int i = 0; i < this.n; i++) {
+                int number = this.grid[i][j];
+                colNumbers.add(number);
+                sum += number;
+            }
 
-	private void generateSolution() {
-		// TODO
+            if(colNumbers.size() != this.n || sum != total) return false;
+        }
 
-	}
+        return true;
+    }
 
-	/*
-	 * Find a solution to the sudoku problem
-	 * 
-	 */
-	public void findSolution() {
+    /*
+     * Generate a random grid solution
+     *
+     */
 
-		// TODO
-	}
+    private void generateSolution() {
+        // TODO
 
-	public static void main(String args[]) {
-		new Sudoku(4).findSolution();
+    }
 
-	}
+    /*
+     * Find a solution to the sudoku problem
+     *
+     */
+    public void findSolution() {
+
+        // TODO
+    }
+
+    public static void main(String args[]) {
+        new Sudoku(4).findSolution();
+
+    }
 }
